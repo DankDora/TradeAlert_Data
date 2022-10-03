8699c364216dfde8abc008adf9463ad81a3d5ada
DankDora <lorigianoty@gmail.com>
2022-10-02 20:52:17 -0400

    Use this file to download data

---
+++ Data Downloader (all high OI tickers) (customizable).py
@@ -0,0 +1,52 @@
+#! python3
+# Data Downloader.py --  This calls the Data & Parser classes and
+			#	 downloads all data for all HIGH OI tickers from .txt
+						
+import tradealertdownloaderclass as datadl
+
+import sys
+import os
+import re
+# ~ Data("XLU").update("intra_volume", amount = 0)
+import time
+# "Update()" class method inputs:
+# ~ def update(self, chart, date = closestTradingDate(), amount = 1):
+
+
+
+
+
+
+with open(r"C:\Users\lorig\Desktop\PythonCode\Projects, Exercises, Programs\Trade Alert Journey\high_OI_tickers.txt") as f:
+	lines = f.readlines()
+
+#try to use (.*)? to make it not-greedy so it takes first found
+tickerRegex = re.compile(r"'([.A-Za-z]*)', ")
+
+tickersList = tickerRegex.findall(lines[0])
+print(len(tickersList))
+
+# ~ print(tickersList)
+
+
+# ~ for ticker in tickersList:
+	# ~ if ticker == 
+
+while(1):
+	counter = 0
+	try:
+		for ticker in tickersList:
+			print(ticker)
+			# ~ ## ~ ticker = "NXTG"
+			datadl.Data(ticker).update(chart = "hist_volatility", amount = 0) 
+			
+	except (IndexError):
+		
+		print("IndexError...trying again")
+		time.sleep(5)
+		continue
+
+	except (TypeError):
+		print("TypeError...trying again")
+		time.sleep(5)
+		continue
